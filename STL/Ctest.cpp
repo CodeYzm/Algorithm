97fe94ae7e42c906d18de8b969b6904c24ebc3ca
yzm <yangzhaomeng991030@gmail.com>
2022-08-26 13:34:24 +0800

    test

--- STL/Ctest.cpp
+++ STL/Ctest.cpp
@@ -1,28 +1,20 @@
-#include<cstdio>
 #include<iostream>
-#include<vector>
-#include<string>
+#include<cstdio>
 using namespace std;
-int cnt = 0;
-static void print() {
-	cout << cnt << endl;
-}
 
-//int main() {
-//	cout << "char : " << sizeof(char) << "字节" << endl;
-//	cout << "unsigned char : " << sizeof(uint8_t) << "字节" << endl;
-//	cout << "w_char : " << sizeof(wchar_t) << "字节" << endl;
-//	cout << "char16_t : " << sizeof(char16_t) << "字节" << endl;
-//	cout << "char32_t : " << sizeof(char32_t) << "字节" << endl;
-//	cout << "short : " << sizeof(short) << "字节" << endl;
-//	cout << "unsigned short : " << sizeof(uint16_t) << "字节" << endl;
-//	cout << "int : " << sizeof(int) << "字节" << endl;
-//	cout << "unsigned int : " << sizeof(uint32_t) << "字节" << endl;
-//	cout << "long : " << sizeof(long) << "字节" << endl;
-//	cout << "long long : " << sizeof(long long) << "字节" << endl;
-//	cout << "unsigned long (long) : " << sizeof(uint64_t) << "字节" << endl;
-//	cout << "float : " << sizeof(float) << "字节" << endl;
-//	cout << "double : " << sizeof(double) << "字节" << endl;
-//	cout << "long double : " << sizeof(long double) << "字节" << endl;
-//	return 0;
-//}
+class W {
+public:
+    int b;
+    int operator*(int a) {
+        return  a + b;
+    }
+};
+
+int main() {
+    W w;
+    w.b = 2;
+    cout << w * 4 << endl;
+    cout << 4 (::*) w << endl;
+    cout << w.operator*(4) << endl;
+    return 0;
+}
