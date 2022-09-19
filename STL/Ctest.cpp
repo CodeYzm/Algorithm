fa01f61ccda1be6071f94d7aa15d895a506dad02
yzm <yangzhaomeng991030@gmail.com>
2022-09-19 23:24:51 +0800

    update

--- STL/Ctest.cpp
+++ STL/Ctest.cpp
@@ -10,11 +10,25 @@ void func() {
 int func(int a) {
     return 0;
 }
+float funce(float x) {
+    return x;
+}
+
 
 int main() {
     //unsigned int a = 2; int b = -3;
     //cout << a + b << endl;
-    int* a;
-    cout << *a << endl;
+    //int* a;
+    //cout << *a << endl;
+    //float(*pf)(float x);
+    //pf = funce;
+    //cout << pf(1.0) << endl;
+    //int* p[4];
+    //p[0] = new int(1);
+    //p[1] = new int(2);
+    //p[2] = new int(2);
+    //p[3] = new int(2);
+    //cout << *p[0] << endl;
+
     return 0;
 }
