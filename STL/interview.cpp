bd2db2a14be6b7d375567b5c8ff923efb37727eb
yzm <yangzhaomeng991030@gmail.com>
2022-10-17 16:59:24 +0800

    update

--- STL/interview.cpp
+++ STL/interview.cpp
@@ -3,8 +3,6 @@
 #include<stack>
 using namespace std;
 
-//int main() {
-//	string s = "1.25+1.3";
-//	cout << getRes(s) << endl;
-//	return 0;
-//}
+int main() {
+
+}
--- STL/smart_ptr_usage.cpp
+++ STL/smart_ptr_usage.cpp
@@ -1,23 +1,16 @@
 #include<iostream>
 using namespace std;
 
-class A {
-public:
-	void func1() {}
-	void func2() {}
-	virtual void func3() {}
-};
-
 void usage_of_shared_ptr() {
 	
 	// 1.创建shared_ptr
 	// 推荐使用make_shared
-	shared_ptr<int>sp1 = make_shared<int>(4);
+	//shared_ptr<int>sp1 = make_shared<int>(4);
 	//shared_ptr<int>sp2 = shared_ptr<int>(new int(4));
 	// 不要用同一个原始指针初始化多个智能指针
-	int* p = new int(4);
-	shared_ptr<int>sp2(p);
-	shared_ptr<int>sp3(p);
+	//int* p = new int(4);
+	//shared_ptr<int>sp2(p);
+	//shared_ptr<int>sp3(p);
 	//cout << sp1 << endl;
 	//cout << bool(sp1) << endl;
 
@@ -31,9 +24,9 @@ void usage_of_shared_ptr() {
 
 	// 3.reset()
 	// 无参数调用，引用计数清0，取消对资源的管理权
-	sp1.reset();
+	//sp1.reset();
 	// 带参数调用，引用计数变为1，获得新的资源管理权
-	sp1.reset(new int(1));
+	//sp1.reset(new int(1));
 
 	// 4.判空
 	//cout << sp1.use_count() << endl;
@@ -49,11 +42,36 @@ void usage_of_shared_ptr() {
 	//int* p2 = new int[2];
 	//shared_ptr<int[]>sp4(p2,deletor);
 
-	weak_ptr<int>sp9(sp1);
-	sp9.
+	//weak_ptr<int>wp1(sp1);
+	//sp1 = wp1.lock();
+	//cout << sp1.use_count() << endl;
+	//sp1.reset();
+	//cout << wp1.expired() << endl;
+
+	unique_ptr<int>up(new int(2));
+	unique_ptr<int>up1 = make_unique<int>(1);
+
+	//cout << *up1 << endl;
+	//cout << *(up1.get()) << endl;
+	//unique_ptr<int>up2 = move(up1);
+	//cout << *up2 << endl;
+	//unique_ptr<int>up3(move(up2));
+	//cout << *up3 << endl;
+
+	//int* p = up1.release();
+	//cout << *up1 << endl;
+	//cout << *p << endl;
+
+	//up.swap(up1);
+	//cout << *up << endl;
+
+
+	int* p = new int(3);
+	up1.reset(p);
+	cout << *up1 << endl;
 }
 
-int main() {
-	usage_of_shared_ptr();
-	return 0;
-}
+//int main() {
+//	usage_of_shared_ptr();
+//	return 0;
+//}
